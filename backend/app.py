ca341c69c161172ab3357787fe9b98b766434baa
Esteban Amores <estebanamoresbarrantes@gmail.com>
2026-06-14 00:22:59 -0600

    Initial Commit

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/api/message")
+def message():
+    return {
+        "message": "Version 1 desplegada por ArgoCD"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
